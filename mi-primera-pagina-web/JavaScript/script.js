bfae7a610b8fcc947c60cb5eefd477e242744ead
SXPSHONGE <135290543+SXPSHONGE@users.noreply.github.com>
2026-09-24 13:09:22 +0200

    Agrego paginas de ciudades con fotos e info, logo e Instagram

--- mi-primera-pagina-web/JavaScript/script.js
+++ mi-primera-pagina-web/JavaScript/script.js
@@ -1,14 +1,10 @@
-function mostrarInfo(ciudad) {
-    alert("Más información sobre " + ciudad + " próximamente.");
-}
-
 function enviarFormulario(event) {
     event.preventDefault();
 
     const nombre = document.getElementById("nombre").value;
 
     document.getElementById("respuesta").textContent =
-        "Gracias " + nombre + ", tu mensaje ha sido enviado correctamente.";
+        "¡Gracias " + nombre + ", tu mensaje ha sido enviado correctamente!";
 
     event.target.reset();
 }
